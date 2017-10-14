2acc1d9fdb126d168bb8dfa28eeeee6beeea7a47
Sarmad Hashmi <sarmadhashmi@gmail.com>
2017-10-14 14:47:26 -0400

    fix home controller issue

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
     session = ShopifyAPI::Session.new(shop_info.shopify_domain, shop_info.shopify_token)
     ShopifyAPI::Base.activate_session(session)
     @products = ShopifyAPI::Product.all
+    puts @products
     response.headers['X-Frame-Options'] = "ALLOW-FROM " << params[:shop]
     puts @wishlist
   end
