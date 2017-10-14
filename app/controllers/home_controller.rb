5252d0b3449aaa0929819ae824312dc3c81a3846
KarimChukfeh <kchuk030@uottawa.ca>
2017-10-14 15:26:24 -0400

    comment out shop info

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
     @wishlist = WishlistItem.getUniqueProductsByStore(params[:shop])
-    shop_info = Shop.where(shopify_domain: params[:shop]).first
-    session = ShopifyAPI::Session.new(shop_info.shopify_domain, shop_info.shopify_token)
+    #shop_info = Shop.where(shopify_domain: params[:shop]).first
+    #session = ShopifyAPI::Session.new(shop_info.shopify_domain, shop_info.shopify_token)
     ShopifyAPI::Base.activate_session(session)
     @products = ShopifyAPI::Product.all
     puts @products
