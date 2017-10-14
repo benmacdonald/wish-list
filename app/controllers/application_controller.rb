9e96d564b8f9f1032febc17d0c12e2e7099896b1
Sarmad Hashmi <sarmadhashmi@gmail.com>
2017-10-14 14:11:17 -0400

    Set X-Frame-Options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :allow_iframe_requests
-
-  def allow_iframe_requests
-    unless Rails.env == 'production'
-      print("yes")
-      response.headers.delete('X-Frame-Options')
-    end
-  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @wishlist = WishlistItem.getUniqueProductsByStore(params[:shop])
+    response.headers['X-Frame-Options'] = "ALLOW-FROM " << shop_info.shopify_domain
     puts @wishlist
   end
 end
