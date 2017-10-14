aaad991b41164675fcc98c9aea80f8c7df01d74e
Sarmad Hashmi <sarmadhashmi@gmail.com>
2017-10-14 15:40:15 -0400

    update home controller again

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
    	@products = ShopifyAPI::Product.find(:all)
-  	@webhooks = ShopifyAPI::Webhook.find(:all)
-  	response.headers['X-Frame-Options'] = "ALLOW-FROM " << ShopifyAPI::Shop.current.domain
+   	domain = ShopifyAPI::Shop.current.domain
+   	@wishlist = WishlistItem.getUniqueProductsByStore(domain)
+  	response.headers['X-Frame-Options'] = "ALLOW-FROM " << domain
   end
 end
