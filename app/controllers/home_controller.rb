89aaa659ec251762e07339d5c87efaa9046eee33
Sarmad Hashmi <sarmadhashmi@gmail.com>
2017-10-14 16:06:56 -0400

    Link image to edit in shop

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
    	@products = ShopifyAPI::Product.find(:all)
+   	@shop = ShopifyAPI::Shop.current
    	domain = ShopifyAPI::Shop.current.domain
    	@wishlist = WishlistItem.getUniqueProductsByStore(domain)
   	response.headers['X-Frame-Options'] = "ALLOW-FROM " << domain
