a9a013429ae1e79a3b3a732c851f34048fac35df
Sarmad Hashmi <sarmadhashmi@gmail.com>
2017-10-14 14:32:43 -0400

    Update home controller to return products

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def index
     @wishlist = WishlistItem.getUniqueProductsByStore(params[:shop])
+    shop_info = Shop.where(shopify_domain: params[:shop]).first
+    session = ShopifyAPI::Session.new(shop_info.shopify_domain, shop_info.shopify_token)
+    ShopifyAPI::Base.activate_session(session)
+    @products = ShopifyAPI::Product.all
     response.headers['X-Frame-Options'] = "ALLOW-FROM " << params[:shop]
     puts @wishlist
   end
